f5b336c49545ad0e1dab3a46e49432a2481479de
chabrik abdellatif <abdellatif.chabrik.fycomputing@gmail.com>
2023-03-12 23:24:35 +0100

    add delete usecase

---
+++ src/book/_use-cases/delete_book/delete-book.use-case.ts
@@ -0,0 +1,24 @@
+import { Injectable,InternalServerErrorException,NotFoundException } from '@nestjs/common';
+import { MyError } from 'src/_core/_business/baseError.error';
+import { BookServiceImplementation } from 'src/book/_business/book.service.implementation';
+
+@Injectable()
+export class DeleteBookUseCase {
+    constructor(private readonly bookService:BookServiceImplementation){}
+
+    public async deleteBook(bookId:string):Promise<MyError | boolean> {
+        const result =  await this.bookService.deleteById(bookId);
+        if(result.isRight()){
+            return result.value
+        }
+        else if(result.isLeft()){
+           switch(result.value.code){
+            case 404:
+                throw new NotFoundException(result.value);
+            default:
+                throw new InternalServerErrorException(result.value);
+            }
+           
+        }
+    }
+}
