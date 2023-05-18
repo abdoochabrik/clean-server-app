1c787a3b2c15f850c31ef274ec9e1c01394b63e6
chabrik abdellatif <abdellatif.chabrik.fycomputing@gmail.com>
2023-05-18 19:33:03 +0100

    feat: :lock: secure secret routes

--- src/secret/_use-cases/secret.controller.ts
+++ src/secret/_use-cases/secret.controller.ts
@@ -1,5 +1,8 @@
 
-import { Controller, Get,Post,Body} from '@nestjs/common'
+import { Controller, Get,Post,Body,UseGuards} from '@nestjs/common'
+import { Role } from '../../role/_business/role.enum';
+import { Roles } from '../../role/_business/roles.decorator';
+import { RolesGuard } from '../../role/_business/roles.guard';
 import { MyError } from '../../_core/_business/baseError.error';
 import { SecretModel } from '../_business/secret.model';
 import { CreateSecretUseCase } from './add-secret/add-secret.use-case';
@@ -7,6 +10,8 @@ import { CreateSecretRequestDto } from './add-secret/create-secret-request.dto';
 import { GetSecretRequestDto } from './get-secret/get-secret.request.dto';
 import { GetSecretUseCase } from './get-secret/get-secret.use-case';
 
+@Roles(Role.Admin)
+@UseGuards(RolesGuard) 
 @Controller('/secret')
 export class SecretController {
     constructor(private readonly createSecretUseCase:CreateSecretUseCase, 
