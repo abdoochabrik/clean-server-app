488496aa84e8e7b82e47b00d26959ac3e6a2d7df
chabrik abdellatif <abdellatif.chabrik.fycomputing@gmail.com>
2023-05-18 19:36:55 +0100

    feat: :lock: add apigateWay security guard

--- src/secret/_use-cases/secret.controller.ts
+++ src/secret/_use-cases/secret.controller.ts
@@ -1,5 +1,6 @@
 
 import { Controller, Get,Post,Body,UseGuards} from '@nestjs/common'
+import { apiGateWayGuard } from '../../authentication/_business/apiGateWay.guard';
 import { Role } from '../../role/_business/role.enum';
 import { Roles } from '../../role/_business/roles.decorator';
 import { RolesGuard } from '../../role/_business/roles.guard';
@@ -10,6 +11,7 @@ import { CreateSecretRequestDto } from './add-secret/create-secret-request.dto';
 import { GetSecretRequestDto } from './get-secret/get-secret.request.dto';
 import { GetSecretUseCase } from './get-secret/get-secret.use-case';
 
+@UseGuards(apiGateWayGuard)
 @Roles(Role.Admin)
 @UseGuards(RolesGuard) 
 @Controller('/secret')
