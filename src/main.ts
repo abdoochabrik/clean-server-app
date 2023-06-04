860f4adbc2c80f98f635cf84cfdaf07acb1b66b2
chabrik abdellatif <abdellatif.chabrik.fycomputing@gmail.com>
2023-06-04 14:26:28 +0100

    test: :test_tube: configure unit tests env

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
